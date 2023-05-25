9734c7269af0f45cfb6030e637066e5f7f2efd84
multimokia <multimokia3@gmail.com>
2023-05-24 20:39:32 -0400

    fix up full range of fonts in tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import './App.css'
-import { TarotCard } from './components/TarotCard'
 import { ArchTarotFrame } from './components/TarotFrames/ArchTarotFrame'
 import { BeadCurtainTarotFrame } from './components/TarotFrames/BeadCurtainTarotFrame'
 import { CrescentTarotFrame } from './components/TarotFrames/CrescentTarotFrame'
@@ -9,26 +8,29 @@ import { SoundwaveTarotFrame } from './components/TarotFrames/SoundwaveTarotFram
 function App() {
     return (
       <div className="w-full h-full starry text-center flex flex-col items-center">
-        <h1 className='glow animate-pulse-slow text-9xl text-tarot-50'>Weekly Stream Schedule</h1>
+        <h1 className='glow animate-pulse-slow text-9xl text-tarot-50 font-semibold'>Weekly Stream Schedule</h1>
         <p className="text-2xl text-tarot-300 mb-12">
           Twitch.tv/lewn_atic
         </p>
         <div className="w-11/12 h-4/6 flex flex-row items-center space-x-7 mt-5">
-          <TarotCard className="self-start flex-grow" frame={<CrescentTarotFrame className="fill-tarot-300 w-full hover:fill-tarot-100 transition-colors"/>}>
-            <p>card1</p>
-          </TarotCard>
-          <TarotCard className="self-end flex-grow" frame={<ArchTarotFrame className="fill-tarot-300 w-full"/>}>
+          <CrescentTarotFrame className="self-start flex-grow fill-tarot-300 w-full hover:fill-tarot-500 transition-colors">
+            <div className="flex flex-col items-center justify-center h-full m-4">
+              <p className="text-4xl text-tarot-50 glow">Tues</p>
+              <p className="text-2xl text-tarot-100">May 23rd at 5PM EST</p>
+            </div>
+          </CrescentTarotFrame>
+          <ArchTarotFrame className="self-end flex-grow fill-tarot-300 w-full hover:fill-tarot-500 transition-colors">
             <p>card2</p>
-          </TarotCard>
-          <TarotCard className="self-start flex-grow" frame={<SoundwaveTarotFrame className="fill-tarot-300 w-full"/>}>
+          </ArchTarotFrame>
+          <SoundwaveTarotFrame className="self-start flex-grow fill-tarot-300 w-full hover:fill-tarot-500 transition-colors">
             <p>card3</p>
-          </TarotCard>
-          <TarotCard className="self-end flex-grow" frame={<FancyFrameTarotFrame className="fill-tarot-300 w-full"/>}>
-            card4
-          </TarotCard>
-          <TarotCard className="self-start flex-grow" frame={<BeadCurtainTarotFrame className="fill-tarot-300 w-full"/>}>
+          </SoundwaveTarotFrame>
+          <FancyFrameTarotFrame className="fill-tarot-300 w-full self-end flex-grow hover:fill-tarot-500 transition-colors">
+            Card4
+          </FancyFrameTarotFrame>
+          <BeadCurtainTarotFrame className="self-start flex-grow fill-tarot-300 w-full hover:fill-tarot-500 transition-colors">
             card5
-          </TarotCard>
+          </BeadCurtainTarotFrame>
         </div>
       </div>
     )
