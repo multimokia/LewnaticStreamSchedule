53f71b87f3ac2c112afa74e8a2a9f3eab52e0a31
multimokia <multimokia3@gmail.com>
2023-06-03 19:56:56 -0400

    h-full

--- src/components/PlaceholderCardContents/HandsHoldingTarotCards.tsx
+++ src/components/PlaceholderCardContents/HandsHoldingTarotCards.tsx
@@ -3,7 +3,7 @@ import { TarotCardsGraphic } from "@/components/Graphics/TarotCardsGraphic";
 
 export function HandsHoldingTarotCards() {
   return (
-    <div className="flex flex-col items-center justify-center m-4">
+    <div className="flex flex-col items-center justify-center m-4 h-full">
       <TarotCardsGraphic className="w-2/4"/>
       <SparkingHandsGraphic className="w-3/4"/>
     </div>
